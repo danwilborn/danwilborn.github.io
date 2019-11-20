a705fd1685567de2ae8c799cba28fde56cfb5c28
Daniel Wilborn <daniel.wilborn@perficient.com>
2019-11-20 09:11:12 -0600

    dev init

---
+++ precache-manifest.ef0ea519900805e4aa0a98930b7cef88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5252cc954574cf305933063a4422f3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "183ad99bf03345a650c7",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "abc0adc2a7fbdefdf82d",
+    "url": "/static/css/main.9de5afda.chunk.css"
+  },
+  {
+    "revision": "183ad99bf03345a650c7",
+    "url": "/static/js/2.4e16b98b.chunk.js"
+  },
+  {
+    "revision": "abc0adc2a7fbdefdf82d",
+    "url": "/static/js/main.165df39e.chunk.js"
+  },
+  {
+    "revision": "46edef803040b02a650f",
+    "url": "/static/js/runtime-main.d99ce381.js"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/static/media/GothamMedium.77171d8f.ttf"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/app/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './service-worker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
