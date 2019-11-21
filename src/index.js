1de6e2168ce0a0ab391fe6f87be52be6ebe8a00e
Daniel Wilborn <daniel.wilborn@perficient.com>
2019-11-21 13:56:45 -0600

    IE 11 working!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import * as serviceWorker from './service-worker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
